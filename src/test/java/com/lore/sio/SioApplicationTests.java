762215e72c0c602b763fe3b5be7cf7f12cca7ddf
António Ricardo Cuinica <91435493+AntonioCuinica@users.noreply.github.com>
2023-12-27 14:48:12 +0200

    first commit with all files

---
+++ src/test/java/com/lore/sio/SioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lore.sio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
